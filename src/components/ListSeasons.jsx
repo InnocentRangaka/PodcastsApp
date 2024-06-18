92efcc473e3df9f1e0428bf0f6f0ec8f1d1e8b2a
Innocent Rangaka <itrangaka@gmail.com>
2024-06-18 20:46:48 +0200

    Refactor ListSeasons component: updated CSS, changed function signature, and modified JSX structure

--- src/components/ListSeasons.jsx
+++ src/components/ListSeasons.jsx
@@ -4,69 +4,70 @@ import PropTypes from 'prop-types';
 // import { Img } from 'react-image';
 import { encodeText, decodeText } from '../utils/textUtils';
 
-export default function ListSeasons({ title, podcastsObject }) {
-  ListPodcasts.propTypes = {
-    // Title is required and must be a string
-    title: PropTypes.string.isRequired,
-    // podcastsObject is required and must be an object
-    podcastsObject: PropTypes.instanceOf(Object).isRequired,
+export default function ListSeasons({ currentSeasons }) {
+    ListSeasons.propTypes = {
+    // ListSeasons is required and must be an object
+    currentSeasons: PropTypes.instanceOf(Object).isRequired,
   };
+  
+  const seasons = currentSeasons || []; // Use empty array for default
 
-  const typeName = title ? title.toLowerCase() : '';
-  const podcasts = podcastsObject || []; // Use empty array for default
-  if (podcasts) {
-    return (
-      <section>
-        <div className="section-header">
-          <h2>
-            {(typeName && typeName.charAt(0).toUpperCase() + typeName.slice(1))}
-            {' '}
-            podcasts
-          </h2>
-        </div>
-        <div className="section-slider">
-
-          <div className="section-slider">
-            {podcasts.map((podcast) => (
-              <div key={podcast.id} className="section-slider-item">
-                <div className="slider-card">
-                  <div className="card-link" />
-                  {podcast?.image && (
-                  <img
-                    src={podcast.image}
-                    className="card-image"
-                    alt={podcast.title}
-                    // fallback={<span>Loading...</span>} <- // use Img instead of img
-                    loading="lazy"
-                  />
-                  )}
-
-                  <div className="card-footer">
-                    <div className="card-footer-content">
-                      <Link
-                        to={`/show/${encodeText(podcast.title)}`}
-                        className="card-footer-link overflow-wrap"
-                        title={podcast.title}
-                        state={{
-                          show: {
-                            id: `${podcast.id}`,
-                            title: `${podcast.title}`,
-                          },
-                        }}
-                      >
-                        <span className="">
-                          {decodeText(podcast.title)}
-                        </span>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            ))}
-
-          </div>
-        </div>
-      </section>
-    );
+  function getSpan(text, number){
+    return number && `<span>${text} ${number}</span>`;
   }
+  
+    return (
+        <div className='show-list-container'>
+                
+            <div className="show-list-header">
+            <h4>
+                {/* {(typeName && typeName.charAt(0).toUpperCase() + typeName.slice(1))}
+                {' '} */}
+                Seasons
+            </h4>
+            </div>
+            
+            <div className='show-list'>
+            <div className='show-list-content'>
+            {seasons && seasons.map((season) => (
+                <div key={season.id}
+                className='show-list-item padding-0'>
+                    <div className='show-list-item-left'>
+                        <div className='show-list-item-index'>
+                        {/* <div className='show-list-item-thumbnail-overlay'>
+                            sdsd
+                        </div> */}
+                        <div className='show-list-item-index-content'>
+                            <div className='show-list-item-play-button-container'>
+                            <div className='show-list-item-play-button'>
+                                <div className='show-list-item-play-button-icon'>
+                                <div className='show-list-item-play-button'>
+                                    {season.season}
+                                </div>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div className='show-list-item-center'>
+                        <div className='show-list-item-title'>
+                        <Link
+                        className='show-list-item-link'
+                        spellCheck="false"
+                        >{season.title}</Link>
+                        </div>
+                        <div className='show-list-item-text'>
+                        {season?.episodes?.length && (<span>Season {season.season}</span>)}
+                        {season?.episodes?.length && (<span>{season.episodes.length} Episodes</span>)}
+
+                        </div>
+                    </div>
+                </div>
+ 
+            ))}
+            </div>
+            </div>
+        </div>
+    );
 }
