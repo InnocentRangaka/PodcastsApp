94e68dd9985e33fedc01be30c1eef0947336ed27
Innocent Rangaka <itrangaka@gmail.com>
2024-06-20 10:52:24 +0200

    Added new CSS classes and updated DescriptionLayout component with new class names and IconButton

--- src/components/DescriptionLayout.jsx
+++ src/components/DescriptionLayout.jsx
@@ -14,13 +14,13 @@ const DescriptionLayout = ({ description }) => {
   return (
     <div className="show-description flex-row">
       <div className='show-description-title'>
-            <IconButton onClick={handleToggle}>
+            <IconButton onClick={handleToggle} className='collapse-button'>
                 {isOpen ? <ExpandLessIcon /> : <ExpandMoreIcon />}
             </IconButton>
             <span>Description</span>
       </div>
       <Collapse in={isOpen}>
-        <Typography variant="body2" color="textSecondary" component="p" className="">
+        <Typography variant="body2" component="p" className="show-description-p">
             {description}
         </Typography>
       </Collapse>
